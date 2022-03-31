8d0f165433691323c92b7cf85c9a57aabd7ae9d7
Daniel Soto <dasg.1993@gmail.com>
2022-03-30 19:13:50 -0500

    [feature] - create button confirm cart & integrate firebase realtime database in cart view

--- src/screens/cart/index.js
+++ src/screens/cart/index.js
@@ -1,14 +1,20 @@
 import React from "react";
-import { View, Text, FlatList } from "react-native";
-import { connect, useSelector } from "react-redux";
+import { View, Text, FlatList, Button } from "react-native";
+import { connect, useSelector, useDispatch } from "react-redux";
 import CartItem from '../../components/molecules/cart-item/index'
+import { confirmCart, removeItem } from "../../store/actions/cart.action";
 import { styles } from "./styles";
 
 const Cart = ({ navigation }) => {
+    const dispatch = useDispatch();
     const items = useSelector(state => state.cart.items)
     const total = useSelector(state => state.cart.total)
     
-    const handleDeleteItem = (id) => console.log(id)
+    const handleDeleteItem = (id) => dispatch(removeItem(id))
+
+    const handleConfirmCart = () => {
+        dispatch(confirmCart(items, total))
+    }
     
     const renderItem = ({ item }) => <CartItem item={item} onDelete={handleDeleteItem} />
 
@@ -21,6 +27,7 @@ const Cart = ({ navigation }) => {
                     renderItem={renderItem}
                 />
             </View>
+            <Button title="Confirm" onPress={() => handleConfirmCart()} color='#212121'/>
             <View style={styles.footer}>
                 <Text style={styles.total}>Total:</Text>
                 <Text style={styles.totalPrice}>${total}</Text>
