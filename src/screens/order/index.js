b9953a9e50ec1ba9e64a9eae4e06ccb3c43761d5
Daniel Soto <dasg.1993@gmail.com>
2022-04-06 18:55:18 -0500

    [feature] - create order actions to get and delete orders

--- src/screens/order/index.js
+++ src/screens/order/index.js
@@ -1,13 +1,44 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useEffect } from "react";
+import { View, Text, FlatList } from "react-native";
+import { useDispatch, connect, useSelector } from "react-redux";
+import OrderItem from "../../components/molecules/order-item";
+import { getOrders, removeOrder } from "../../store/actions/order.action";
 import { styles } from "./styles";
 
 const Order = () => {
+    const dispatch = useDispatch();
+    const userId = useSelector(state => state.auth.userId);
+    const orders = useSelector(state => state.order.orders);
+
+    useEffect(() => {
+        dispatch(getOrders(userId));
+    }, []);
+
+    const onDelete = (id) => {
+        dispatch(removeOrder(id));
+    }
+
+    const onDetails = (id) => {
+        console.log(id);
+    }
+
+    const renderItem = ({ item }) => (
+        <OrderItem
+            data={item}
+            onDelete={onDelete}
+            onDetails={onDetails}
+        />
+    )
+
     return (
         <View style={styles.container}>
-            <Text>Order</Text>
+            <FlatList
+                data={orders}
+                renderItem={renderItem}
+                keyExtractor={item => item.id.toString()}
+            />
         </View>
     )
 }
 
-export default Order;
+export default connect()(Order);
--- src/store/index.js
+++ src/store/index.js
@@ -3,13 +3,15 @@ import categoryReducer from "./reducers/category.reducer";
 import productReducer from "./reducers/product.reducer";
 import cartReducer from "./reducers/cart.reducer";
 import authReducer from "./reducers/auth.reducer";
+import orderReducer from "./reducers/order.reducer";
 import thunk from 'redux-thunk'
 
 const rootReducer = combineReducers({
     categories: categoryReducer,
     products: productReducer,
     cart: cartReducer,
-    auth: authReducer
+    auth: authReducer,
+    order: orderReducer
 });
 
 export default createStore(rootReducer, applyMiddleware(thunk));
